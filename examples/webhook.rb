22bcf9ee93f9b142778d4b43d1b59e635402f751
Michael Juster <maj@crowdflower.com>
2012-05-21 17:01:10 -0700

    updating example

--- examples/webhook.rb
+++ examples/webhook.rb
@@ -7,9 +7,9 @@ require 'base64'
 PKey = OpenSSL::PKey::RSA.new(File.read(File.join(File.dirname(__FILE__), "webhook_public.pem")))
 
 post "/rtfm_webhook" do
-  signature = env['X-CrowdFlower-Signature']
+  signature = env['HTTP_X_CROWDFLOWER_SIGNATURE']
   body = request.body.read
-  if(@public_key.verify(OpenSSL::Digest::SHA1.new, Base64.decode64(signature), body))
+  if(PKey.verify(OpenSSL::Digest::SHA1.new, Base64.decode64(signature), body))
     payload = MultiJson.load(body)
     #Do something meaningful with the payload here
     logger.debug "SUCCESS"
